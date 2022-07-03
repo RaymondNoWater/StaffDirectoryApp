064e083993406610b94229db1966897ce53c377f
RaymondNoWater <qwertycube@outlook.com>
2022-07-03 15:37:53 +1000

    save changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,34 +7,6 @@ import HomeScreen from './app/screens/HomeScreen';
 import UserProfileScreen from './app/screens/UserProfileScreen';
 import UpdateUserScreen from './app/screens/UpdateUserScreen';
 import AddNewUserScreen from './app/screens/AddNewUserScreen';
-
-//const masterKey = "$2b$10$IZPT0hQ6WGS3T/lqOrfd.OUV.tJz33ey9X3mD2RkHJHpASm.EYrjq";
-//const binId = "62b1ae0d449a1f3821137006";
-//const root = "https://api.jsonbin.io/v3/b/"62b1ae0d449a1f3821137006/latest";
-
-let rawData;
-
-// let req = new XMLHttpRequest();
-// req.onreadystatechange = () => {
-//   if (req.readyState == XMLHttpRequest.DONE) {
-//     var response = req.responseText;
-//     var employeeData = JSON.parse(response);
-//     console.log(employeeData.id);
-//   }
-// };
-// req.open("GET", root, true);
-// req.setRequestHeader("X-Master-Key" : "$2b$10$IZPT0hQ6WGS3T/lqOrfd.OUV.tJz33ey9X3mD2RkHJHpASm.EYrjq";
-// );
-// req.send();
-
-fetch("https://api.jsonbin.io/v3/b/62b1ae0d449a1f3821137006/latest",
-{ method : "GET", headers:{ "X-Master-Key" : "$2b$10$IZPT0hQ6WGS3T/lqOrfd.OUV.tJz33ey9X3mD2RkHJHpASm.EYrjq", "X-Bin-Meta" : false	}})
-.then(res => res.json())
-.then(data => displayTest(data))
-
-function displayTest(testData){
-  console.log(testData[0].id);
-};
 
 
 const Stack = createNativeStackNavigator();
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,20 +1,33 @@
 import {View,Text,Button} from 'react-native';
+import { useState, useEffect } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
 
 function HomeScreen({ navigation }){
-    return (
-      <View>
-        <Text>Home Screen</Text>
-        <Button
-        title="Stand in User Profile"
-        onPress={() => navigation.navigate('UserProfile')}
-        />
-    
-        <Button
-        title="Add New User"
-        onPress={() => navigation.navigate("AddNewUser")}
-        />
-      </View>
-    );
-    }
+  const [isLoading, setLoading] = useState(false);
+  const [users, setUser] = useState([]);
 
+  function getUsers(){
+    fetch("https://api.jsonbin.io/v3/b/62b1ae0d449a1f3821137006/latest",
+    { method : "GET", headers:{ "X-Master-Key" : "$2b$10$IZPT0hQ6WGS3T/lqOrfd.OUV.tJz33ey9X3mD2RkHJHpASm.EYrjq", "X-Bin-Meta" : false	},})
+    .then(res => res.json())
+    .then(data => setUser(data))
+    .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    getUsers();
+  },[])
+
+  
+  
+  return(
+    <View>
+      {isLoading ? <Text>Page is loading</Text> :
+      (
+        <Text>{users[0].name}</Text>
+      )}
+    </View>
+  )
+};
 export default HomeScreen;
